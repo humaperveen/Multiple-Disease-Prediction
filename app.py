a6912f6195e8949bad183c3068ebafc7a70b9c82
humaperveen <97752930+humaperveen@users.noreply.github.com>
2024-12-17 22:49:21 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,381 @@
+import streamlit as st 
+import pickle 
+import os
+from streamlit_option_menu import option_menu
+
+st.set_page_config(page_title="Mulitple Disease Prediction",layout="wide", page_icon="👨‍🦰🤶")
+
+working_dir = os.path.dirname(os.path.abspath(__file__))
+
+diabetes_model = pickle.load(open(f'{working_dir}/saved_models/diabetes.pkl','rb'))
+heart_disease_model = pickle.load(open(f'{working_dir}/saved_models/heart.pkl','rb'))
+kidney_disease_model = pickle.load(open(f'{working_dir}/saved_models/kidney.pkl','rb'))
+parkinsons_model = pickle.load(open(f'{working_dir}/saved_models/parkinsons_model.pkl','rb'))
+
+NewBMI_Overweight=0
+NewBMI_Underweight=0
+NewBMI_Obesity_1=0
+NewBMI_Obesity_2=0 
+NewBMI_Obesity_3=0
+NewInsulinScore_Normal=0 
+NewGlucose_Low=0
+NewGlucose_Normal=0 
+NewGlucose_Overweight=0
+NewGlucose_Secret=0
+
+with st.sidebar:
+    selected = option_menu("Mulitple Disease Prediction", 
+                ['Diabetes Prediction',
+                 'Heart Disease Prediction',
+                 'Kidney Disease Prediction',
+                'Parkinsons Disease Prediction'],
+                 menu_icon='hospital-fill',
+                 icons=['activity','heart', 'person', 'gear'],
+                 default_index=0)
+    # LinkedIn and GitHub URLs
+    linkedin_url = "https://www.linkedin.com/in/huma-perveen-b6549558"
+    github_url = "https://github.com/humaperveen"
+
+    linkedin_icon = "https://cdn-icons-png.flaticon.com/512/174/174857.png"  # LinkedIn Icon
+    github_icon = "https://cdn-icons-png.flaticon.com/512/25/25231.png"     # GitHub Icon
+    
+    footer = """
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <footer>
+        <div style='visibility: visible;margin-top:7rem;justify-content:center;display:flex;'>
+            <p style="font-size:1.1rem;"> Created By:
+                &nbsp;
+                <a href="https://www.linkedin.com/in/huma-perveen-b6549558" target="_blank">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" class="bi bi-linkedin" viewBox="0 0 16 16">
+                        <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                    </svg>          
+                </a>
+                &nbsp;
+                <a href="https://github.com/humaperveen" target="_blank">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="black" class="bi bi-github" viewBox="0 0 16 16">
+                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                    </svg>
+                </a>
+            </p>
+        </div>
+    </footer>
+    """
+    st.sidebar.markdown(
+#         f"""
+#         <a href="{linkedin_url}" target="_blank">
+#             <img src="{linkedin_icon}" width="30"/> LinkedIn
+#         </a><br>
+#         <a href="{github_url}" target="_blank">
+#             <img src="{github_icon}" width="30"/> GitHub
+#         </a>
+#         """,
+        footer,
+        unsafe_allow_html=True
+    )
+   
+st.title("Multiple Disease Prediction System Using Machine Learning")
+
+if selected == 'Diabetes Prediction':
+    st.subheader("Diabetes Prediction")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        Pregnancies = st.text_input("Number of Pregnancies")
+    with col2:
+        Glucose = st.text_input("Glucose Level")
+    with col3:
+        BloodPressure = st.text_input("BloodPressure Value")
+    with col1:
+        SkinThickness = st.text_input("SkinThickness Value")
+    with col2:
+        Insulin = st.text_input("Insulin Value")
+    with col3:
+        BMI = st.text_input("BMI Value")
+    with col1:
+        DiabetesPedigreeFunction = st.text_input("DiabetesPedigreeFunction Value")
+    with col2:
+        Age = st.text_input("Age")
+    diabetes_result = ""
+    if st.button("Diabetes Test Result"):
+        if float(BMI)<=18.5:
+            NewBMI_Underweight = 1
+        elif 18.5 < float(BMI) <=24.9:
+            pass
+        elif 24.9<float(BMI)<=29.9:
+            NewBMI_Overweight =1
+        elif 29.9<float(BMI)<=34.9:
+            NewBMI_Obesity_1 =1
+        elif 34.9<float(BMI)<=39.9:
+            NewBMI_Obesity_2=1
+        elif float(BMI)>39.9:
+            NewBMI_Obesity_3 = 1
+        
+        if 16<=float(Insulin)<=166:
+            NewInsulinScore_Normal = 1
+
+        if float(Glucose)<=70:
+            NewGlucose_Low = 1
+        elif 70<float(Glucose)<=99:
+            NewGlucose_Normal = 1
+        elif 99<float(Glucose)<=126:
+            NewGlucose_Overweight = 1
+        elif float(Glucose)>126:
+            NewGlucose_Secret = 1
+
+        user_input=[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,
+                    BMI,DiabetesPedigreeFunction,Age, NewBMI_Underweight,
+                    NewBMI_Overweight,NewBMI_Obesity_1,
+                    NewBMI_Obesity_2,NewBMI_Obesity_3,NewInsulinScore_Normal, 
+                    NewGlucose_Low,NewGlucose_Normal, NewGlucose_Overweight,
+                    NewGlucose_Secret]
+        
+        user_input = [float(x) for x in user_input]
+        prediction = diabetes_model.predict([user_input])
+        if prediction[0]==1:
+            diabetes_result = "The person has diabetic"
+        else:
+            diabetes_result = "The person has no diabetic"
+    st.success(diabetes_result)
+
+if selected == 'Heart Disease Prediction':
+    st.subheader("Heart Disease Prediction")
+    col1, col2, col3  = st.columns(3)
+
+    with col1:
+        age = st.text_input("Age")
+    with col2:
+        sex = st.text_input("Sex")
+    with col3:
+        cp = st.text_input("Chest Pain Types")
+    with col1:
+        trestbps = st.text_input("Resting Blood Pressure")
+    with col2:
+        chol = st.text_input("Serum Cholestroal in mg/dl")
+    with col3:
+        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
+    with col1:
+        restecg = st.text_input('Resting Electrocardiographic results')
+
+    with col2:
+        thalach = st.text_input('Maximum Heart Rate achieved')
+
+    with col3:
+        exang = st.text_input('Exercise Induced Angina')
+
+    with col1:
+        oldpeak = st.text_input('ST depression induced by exercise')
+
+    with col2:
+        slope = st.text_input('Slope of the peak exercise ST segment')
+
+    with col3:
+        ca = st.text_input('Major vessels colored by flourosopy')
+
+    with col1:
+        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+    heart_disease_result = ""
+    if st.button("Heart Disease Test Result"):
+        user_input = [age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]
+        user_input = [float(x) for x in user_input]
+        prediction = heart_disease_model.predict([user_input])
+        if prediction[0]==1:
+            heart_disease_result = "This person is having heart disease"
+        else:
+            heart_disease_result = "This person does not have any heart disease"
+    st.success(heart_disease_result)
+
+if selected == 'Kidney Disease Prediction':
+    
+    st.subheader("Kidney Disease Prediction")
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        age = st.text_input('Age')
+
+    with col2:
+        blood_pressure = st.text_input('Blood Pressure')
+
+    with col3:
+        specific_gravity = st.text_input('Specific Gravity')
+
+    with col4:
+        albumin = st.text_input('Albumin')
+
+    with col5:
+        sugar = st.text_input('Sugar')
+
+    with col1:
+        red_blood_cells = st.text_input('Red Blood Cell')
+
+    with col2:
+        pus_cell = st.text_input('Pus Cell')
+
+    with col3:
+        pus_cell_clumps = st.text_input('Pus Cell Clumps')
+
+    with col4:
+        bacteria = st.text_input('Bacteria')
+
+    with col5:
+        blood_glucose_random = st.text_input('Blood Glucose Random')
+
+    with col1:
+        blood_urea = st.text_input('Blood Urea')
+
+    with col2:
+        serum_creatinine = st.text_input('Serum Creatinine')
+
+    with col3:
+        sodium = st.text_input('Sodium')
+
+    with col4:
+        potassium = st.text_input('Potassium')
+
+    with col5:
+        haemoglobin = st.text_input('Haemoglobin')
+
+    with col1:
+        packed_cell_volume = st.text_input('Packet Cell Volume')
+
+    with col2:
+        white_blood_cell_count = st.text_input('White Blood Cell Count')
+
+    with col3:
+        red_blood_cell_count = st.text_input('Red Blood Cell Count')
+
+    with col4:
+        hypertension = st.text_input('Hypertension')
+
+    with col5:
+        diabetes_mellitus = st.text_input('Diabetes Mellitus')
+
+    with col1:
+        coronary_artery_disease = st.text_input('Coronary Artery Disease')
+
+    with col2:
+        appetite = st.text_input('Appetitte')
+
+    with col3:
+        pedal_edema = st.text_input('Pedal Edema')
+    with col4:
+        anemia = st.text_input('Anemia')
+
+    # code for Prediction
+    kindey_diagnosis = ''
+
+    # creating a button for Prediction    
+    if st.button("Kidney's Test Result"):
+
+        user_input = [age, blood_pressure, specific_gravity, albumin, sugar,
+       red_blood_cells, pus_cell, pus_cell_clumps, bacteria,
+       blood_glucose_random, blood_urea, serum_creatinine, sodium,
+       potassium, haemoglobin, packed_cell_volume,
+       white_blood_cell_count, red_blood_cell_count, hypertension,
+       diabetes_mellitus, coronary_artery_disease, appetite,
+       pedal_edema, anemia]
+
+        user_input = [float(x) for x in user_input]
+
+        prediction = kidney_disease_model.predict([user_input])
+
+        if prediction[0] == 1:
+            kindey_diagnosis = "The person has Kidney's disease"
+        else:
+            kindey_diagnosis = "The person does not have Kidney's disease"
+    st.success(kindey_diagnosis)
+    
+# Parkinson's Prediction Page
+if selected == "Parkinsons Disease Prediction":
+    
+    # page title
+    st.subheader("Parkinson's Disease Prediction")
+    
+    col1, col2, col3, col4, col5 = st.columns(5)  
+    
+    with col1:
+        fo = st.text_input('MDVP:Fo(Hz)')
+        
+    with col2:
+        fhi = st.text_input('MDVP:Fhi(Hz)')
+        
+    with col3:
+        flo = st.text_input('MDVP:Flo(Hz)')
+        
+    with col4:
+        Jitter_percent = st.text_input('MDVP:Jitter(%)')
+        
+    with col5:
+        Jitter_Abs = st.text_input('MDVP:Jitter(Abs)')
+        
+    with col1:
+        RAP = st.text_input('MDVP:RAP')
+        
+    with col2:
+        PPQ = st.text_input('MDVP:PPQ')
+        
+    with col3:
+        DDP = st.text_input('Jitter:DDP')
+        
+    with col4:
+        Shimmer = st.text_input('MDVP:Shimmer')
+        
+    with col5:
+        Shimmer_dB = st.text_input('MDVP:Shimmer(dB)')
+        
+    with col1:
+        APQ3 = st.text_input('Shimmer:APQ3')
+        
+    with col2:
+        APQ5 = st.text_input('Shimmer:APQ5')
+        
+    with col3:
+        APQ = st.text_input('MDVP:APQ')
+        
+    with col4:
+        DDA = st.text_input('Shimmer:DDA')
+        
+    with col5:
+        NHR = st.text_input('NHR')
+        
+    with col1:
+        HNR = st.text_input('HNR')
+        
+    with col2:
+        RPDE = st.text_input('RPDE')
+        
+    with col3:
+        DFA = st.text_input('DFA')
+        
+    with col4:
+        spread1 = st.text_input('spread1')
+        
+    with col5:
+        spread2 = st.text_input('spread2')
+        
+    with col1:
+        D2 = st.text_input('D2')
+        
+    with col2:
+        PPE = st.text_input('PPE')
+        
+    
+    
+    # code for Prediction
+    parkinsons_diagnosis = ''
+    
+    # creating a button for Prediction    
+    if st.button("Parkinson's Test Result"):
+        user_input = [fo, fhi, flo, Jitter_percent, Jitter_Abs,
+                      RAP,PPQ,DDP,Shimmer,Shimmer_dB,APQ3,APQ5,
+                      APQ,DDA,NHR,HNR,RPDE,DFA,spread1,spread2,D2,PPE]
+        user_input = [float(x) for x in user_input]
+        parkinsons_prediction = parkinsons_model.predict([user_input])                          
+        
+        if (parkinsons_prediction[0] == 1):
+            parkinsons_diagnosis = "The person has Parkinson's disease"
+        else:
+            parkinsons_diagnosis = "The person does not have Parkinson's disease"
+        
+    st.success(parkinsons_diagnosis)
